a51448e40650ee62e5c949a13157c0e35707a571
Brendan Heberlein <bheberlein@wisc.edu>
2023-03-01 08:23:10 -0600

    Release under GNU GPLv3 license
    HyPro is released under the GNU General Public License Version 3. The source code & package files are free to use and distribute strictly under the terms of this license, and no later version.

--- Classification.py
+++ Classification.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020
+# Nanfeng Liu <nliu58@wisc.edu>
+# Philip Townsend <ptownsend@wisc.edu>
+#
+# Environmental Spectroscopy Laboratory
+# Department of Forest & Wildlife Ecology
+# University of Wisconsin – Madison
+#
+# Licensed under GNU GPLv3
+# See `./LICENSE.txt` for complete terms
 
 """ Functions to do image classifications.
 @author: Nanfeng Liu (nliu58@wisc.edu)
