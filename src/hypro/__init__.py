97a9575196d3aab6f18069bab7945341d3d906c0
Brendan Heberlein <bheberlein@wisc.edu>
2023-03-01 08:37:03 -0600

    Add `.gitignore` & packaging files
    The listed dependencies are likely not hard minimal requirements, but basic package functionalities have been tested with these versions.

---
+++ src/hypro/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020
+# Nanfeng Liu <nliu58@wisc.edu>
+# Philip Townsend <ptownsend@wisc.edu>
+#
+# Environmental Spectroscopy Laboratory
+# Department of Forest & Wildlife Ecology
+# University of Wisconsin – Madison
+#
+# Licensed under GNU GPLv3
+# See `./LICENSE.txt` for complete terms
